823cbeb9a0f01a2bc467f92eabb316a2cbd6304d
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-04-24 16:57:31 +0530

    feat: add fn to generate shared keys

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use rand::Rng;
 
-fn generateSecrets(secret: u64) {
+#[derive(Debug)]
+struct Key {
+    x: u64,
+    y: u64,
+}
+
+fn generateSecrets(secret: u64) -> Vec<Key> {
     let mut rng = rand::thread_rng();
     let factor: u64 = rng.gen();
+    let mut shared_keys: Vec<Key> = Vec::new();
+    let mut i: u64 = 1;
+    loop {
+        let temp_keys: Key = Key {
+            x: i,
+            y: secret + factor * i,
+        };
+        shared_keys.push(temp_keys);
+        i += 1;
+        if i == 5 {
+            break;
+        };
+    }
+    return shared_keys;
 }
 
 fn main() {
-    generateSecrets(64);
+    print!("{:?}", generateSecrets(64));
 }
